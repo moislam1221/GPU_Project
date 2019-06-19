43efcbe8837d8c8e058822c623bc1332809eaa80
Mohammad <moislam@mit.edu>
2019-06-19 14:38:14 -0400

    Added relaxation factor to Jacobi iteration in iterative-methods.h file

--- 2D-domain-decomposition/iterative-methods.h
+++ 2D-domain-decomposition/iterative-methods.h
@@ -6,7 +6,13 @@ __host__ __device__
 float jacobi(const float leftMatrix, const float centerMatrix, const float rightMatrix, const float topMatrix, const float bottomMatrix,
               const float leftX, const float centerX, const float rightX, const float topX, const float bottomX,
               const float centerRhs) {
-    float result = (centerRhs - (leftMatrix * leftX + rightMatrix * rightX + topMatrix * topX + bottomMatrix * bottomX)) / centerMatrix;
+    // Relaxation factor
+    float omega = 1.0;
+
+    // Perform relaxed Jacobi
+    float result = omega * ((centerRhs - (leftMatrix * leftX + rightMatrix * rightX + topMatrix * topX + bottomMatrix * bottomX)) / centerMatrix)
+                   + (1.0 - omega) * centerX;
+                    
     return result;
 }
 
