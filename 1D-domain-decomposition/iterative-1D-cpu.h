7a99ad561898ce4c1a4e7d95fc15c34b1fc9d08c
Mohammad <moislam@mit.edu>
2019-07-10 12:14:22 -0400

    Implemented jacobi and relaxed jacobi as separate methods in iterative-methods. Added a CPU method for Gauss-Seidel to iterative-1D-cpu

--- 1D-domain-decomposition/iterative-1D-cpu.h
+++ 1D-domain-decomposition/iterative-1D-cpu.h
@@ -15,9 +15,9 @@
 #include <string.h>
 #include <utility>
 
-float * iterativeCpu(const float * initX, const float * rhs,
+float * jacobiCpu(const float * initX, const float * rhs,
                   const float * leftMatrix, const float * centerMatrix,
-                  const float * rightMatrix, int nGrids, int nIters, int method)
+                  const float * rightMatrix, int nGrids, int nIters)
 {
     float * x0 = new float[nGrids];
     float * x1 = new float[nGrids];
@@ -28,16 +28,9 @@ float * iterativeCpu(const float * initX, const float * rhs,
             float leftX = x0[iGrid - 1];
             float centerX = x0[iGrid];
             float rightX = x0[iGrid + 1];
-	    if (iIter % 2 == 0) {
-                x1[iGrid] = iterativeOperation(leftMatrix[iGrid], centerMatrix[iGrid],
-                                    rightMatrix[iGrid], leftX, centerX, rightX,
-                                    rhs[iGrid], iGrid, method);
-	    }
-	    else { 
-                x1[iGrid] = iterativeOperation2(leftMatrix[iGrid], centerMatrix[iGrid],
-                                    rightMatrix[iGrid], leftX, centerX, rightX,
-                                    rhs[iGrid], iGrid, method);
-            }
+            x1[iGrid] = jacobi(leftMatrix[iGrid], centerMatrix[iGrid],
+                               rightMatrix[iGrid], leftX, centerX, rightX,
+                               rhs[iGrid]);
         }
         float * tmp = x0; x0 = x1; x1 = tmp;
     }
@@ -45,9 +38,9 @@ float * iterativeCpu(const float * initX, const float * rhs,
     return x0;
 }
 
-int iterativeCpuIterationCount(const float * initX, const float * rhs,
+int jacobiCpuIterationCount(const float * initX, const float * rhs,
                   const float * leftMatrix, const float * centerMatrix,
-                  const float * rightMatrix, int nGrids, float TOL, int method)
+                  const float * rightMatrix, int nGrids, float TOL)
 {
     float * x0 = new float[nGrids];
     float * x1 = new float[nGrids];
@@ -61,16 +54,67 @@ int iterativeCpuIterationCount(const float * initX, const float * rhs,
             float leftX = x0[iGrid - 1];
             float centerX = x0[iGrid];
             float rightX = x0[iGrid + 1];
-	    if (iIter % 2 == 0) {
-                x1[iGrid] = iterativeOperation(leftMatrix[iGrid], centerMatrix[iGrid],
-                                    rightMatrix[iGrid], leftX, centerX, rightX,
-                                    rhs[iGrid], iGrid, method);
+            x1[iGrid] = jacobi(leftMatrix[iGrid], centerMatrix[iGrid],
+                               rightMatrix[iGrid], leftX, centerX, rightX,
+                               rhs[iGrid]);
+        }
+        float * tmp = x0; x0 = x1; x1 = tmp;
+        iIter++;
+        residual = Residual(x0, rhs, leftMatrix, centerMatrix, rightMatrix, nGrids);
+    }
+    int nIters = iIter;
+    delete[] x1;
+    return nIters;
+}
+
+float * gaussSeidelCpu(const float * initX, const float * rhs,
+                  const float * leftMatrix, const float * centerMatrix,
+                  const float * rightMatrix, int nGrids, int nIters)
+{
+    float * x0 = new float[nGrids];
+    float * x1 = new float[nGrids];
+    memcpy(x0, initX, sizeof(float) * nGrids);
+    memcpy(x1, initX, sizeof(float) * nGrids);
+    const float omega = 2.0 / (1.0 + sin(PI / (nGrids - 1)));
+    for (int iIter = 0; iIter < nIters; ++ iIter) {
+        for (int iGrid = 1; iGrid < nGrids-1; ++iGrid) {
+            float leftX = x0[iGrid - 1];
+            float centerX = x0[iGrid];
+            float rightX = x0[iGrid + 1];
+	    if (iIter % 2 == iGrid % 2) {
+                x1[iGrid] = relaxedJacobi(leftMatrix[iGrid], centerMatrix[iGrid],
+                               rightMatrix[iGrid], leftX, centerX, rightX,
+                               rhs[iGrid], omega);
 	    }
-	    else { 
-                x1[iGrid] = iterativeOperation2(leftMatrix[iGrid], centerMatrix[iGrid],
-                                    rightMatrix[iGrid], leftX, centerX, rightX,
-                                    rhs[iGrid], iGrid, method);
-            }
+        }
+        float * tmp = x0; x0 = x1; x1 = tmp;
+    }
+    delete[] x1;
+    return x0;
+}
+
+int gaussSeidelCpuIterationCount(const float * initX, const float * rhs,
+                  const float * leftMatrix, const float * centerMatrix,
+                  const float * rightMatrix, int nGrids, float TOL)
+{
+    float * x0 = new float[nGrids];
+    float * x1 = new float[nGrids];
+    memcpy(x0, initX, sizeof(float) * nGrids);
+    memcpy(x1, initX, sizeof(float) * nGrids);
+    const float omega = 2.0 / (1.0 + sin(PI / (nGrids - 1)));
+
+    float residual = 100.0;
+    int iIter = 0;
+    while (residual > TOL) {
+        for (int iGrid = 1; iGrid < nGrids-1; ++iGrid) {
+            float leftX = x0[iGrid - 1];
+            float centerX = x0[iGrid];
+            float rightX = x0[iGrid + 1];
+	    if (iIter % 2 == iGrid % 2) {
+                x1[iGrid] = relaxedJacobi(leftMatrix[iGrid], centerMatrix[iGrid],
+                               rightMatrix[iGrid], leftX, centerX, rightX,
+                               rhs[iGrid], omega);
+	    }
         }
         float * tmp = x0; x0 = x1; x1 = tmp;
         iIter++;
--- 1D-domain-decomposition/iterative-methods.h
+++ 1D-domain-decomposition/iterative-methods.h
@@ -1,5 +1,18 @@
 enum method_type { JACOBI, GS, SOR };
 
+__host__ __device__
+float jacobi(const float leftMatrix, const float centerMatrix, const float rightMatrix, float leftX, float centerX, float rightX, const float centerRhs)
+{
+    return gridValue = (centerRhs - (leftMatrix * leftX + rightMatrix * rightX)) / centerMatrix;
+}
+
+__host__ __device__
+float relaxedJacobi(const float leftMatrix, const float centerMatrix, const float rightMatrix, float leftX, float centerX, float rightX, const float centerRhs, const float omega)
+{
+    return gridValue = relaxation*((centerRhs - (leftMatrix * leftX + rightMatrix * rightX)) / centerMatrix) + (1.0-relaxation)*centerX;
+}
+
+
 template <typename method_type>
 __host__ __device__
 float iterativeOperation(const float leftMatrix, const float centerMatrix, const float rightMatrix, float leftX, float centerX, float rightX, const float centerRhs, int gridPoint, method_type method)
